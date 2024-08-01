646d250dfe9a9af36983b6224fc97417ff4487a2
Nathael Arki <nath.arki36@gmail.com>
2024-08-01 19:58:43 +0200

    Cards / BG can be switcher using riv. Still using test images.

--- cards/main.js
+++ cards/main.js
@@ -1,11 +1,28 @@
+const contourButton = document.querySelector("#contour")
+const bgButton = document.querySelector("#bg")
+
+
 const r = new rive.Rive({
-    src: "https://krayorn.github.io/AlgoArena-Submissions/cards/superdev_cards.riv",
+    src: "./superdev_cards.riv",
     // OR the path to a discoverable and public Rive asset
     // src: '/public/example.riv',
     canvas: document.getElementById("canvas"),
     autoplay: true,
-    stateMachines: "bumpy",
+    stateMachines: "State Machine 1",
     onLoad: () => {
-      r.resizeDrawingSurfaceToCanvas();
+        // Get the inputs via the name of the state machine
+        const inputs = r.stateMachineInputs('State Machine 1');
+        // Find the input you want to set a value for, or trigger
+        let contourNumber = inputs.find(i => i.name === 'Contour');
+
+        contourButton.onclick = () => {
+            contourNumber.value = contourNumber.value === 1 ? 0 : 1
+        }
+
+        let bgNumber = inputs.find(i => i.name === 'Bg');
+
+        bgButton.onclick = () => {
+            bgNumber.value = bgNumber.value === 1 ? 0 : 1
+        }
     },
 });
