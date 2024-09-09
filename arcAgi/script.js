821e72c3a54b0e78d247bc15666a55d485d47581
Nathael Arki <nath.arki36@gmail.com>
2024-09-09 09:22:26 +0200

    arc agi challenge

---
+++ arcAgi/script.js
@@ -0,0 +1,219 @@
+const puzzle2 = {"train": [{"input": [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]], "output": [[3, 3, 3, 3, 3, 3], [0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 0, 3], [3, 0, 3, 3, 0, 3], [3, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3]]}, {"input": [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]], "output": [[3, 3, 3, 3, 3, 3, 3, 3], [0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3]]}, {"input": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], "output": [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3], [3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 0, 0, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 3, 3, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3], [3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]}, {"input": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], "output": [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3], [3, 0, 3, 0, 0, 0, 0, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 3, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 0, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 3, 3, 3, 3, 0, 3, 0, 3], [3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]}, {"input": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], "output": [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3], [0, 0, 0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 3, 0, 3, 0, 3], [3, 0, 3, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]}], "test": [{"input": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], "output": [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3], [3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 3, 3, 3, 3, 0, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 3, 0, 3, 3, 0, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 3, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 3], [3, 0, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3], [3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3], [3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3], [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3], [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]}]}
+const puzzle1 = {"test": [{"input": [[2, 0, 2, 2, 0], [2, 0, 0, 2, 2], [2, 2, 2, 0, 0], [2, 2, 2, 2, 2], [0, 2, 2, 0, 0], [2, 2, 2, 2, 2], [4, 4, 4, 4, 4], [0, 0, 0, 2, 2], [2, 0, 0, 0, 2], [2, 2, 2, 0, 2], [0, 2, 2, 0, 0], [2, 0, 2, 2, 0], [2, 0, 2, 2, 2]], "output": [[3, 0, 3, 0, 3], [0, 0, 0, 3, 0], [0, 0, 0, 0, 3], [3, 0, 0, 3, 3], [3, 3, 0, 3, 0], [0, 3, 0, 0, 0]]}], "train": [{"input": [[0, 0, 0, 2, 2], [0, 0, 2, 0, 2], [2, 0, 0, 2, 2], [2, 2, 0, 0, 2], [0, 0, 0, 0, 2], [0, 2, 0, 0, 0], [4, 4, 4, 4, 4], [2, 0, 0, 0, 0], [2, 2, 0, 0, 0], [2, 0, 2, 0, 0], [0, 0, 2, 0, 0], [0, 0, 0, 2, 2], [2, 0, 0, 2, 0]], "output": [[3, 0, 0, 3, 3], [3, 3, 3, 0, 3], [0, 0, 3, 3, 3], [3, 3, 3, 0, 3], [0, 0, 0, 3, 0], [3, 3, 0, 3, 0]]}, {"input": [[0, 2, 2, 2, 2], [0, 0, 0, 0, 2], [2, 0, 2, 2, 2], [0, 0, 2, 2, 0], [2, 2, 2, 2, 0], [2, 2, 0, 0, 2], [4, 4, 4, 4, 4], [0, 0, 0, 0, 0], [0, 0, 2, 0, 0], [2, 0, 0, 0, 2], [0, 0, 0, 2, 0], [0, 2, 0, 2, 0], [0, 2, 2, 2, 0]], "output": [[0, 3, 3, 3, 3], [0, 0, 3, 0, 3], [0, 0, 3, 3, 0], [0, 0, 3, 0, 0], [3, 0, 3, 0, 0], [3, 0, 3, 3, 3]]}, {"input": [[2, 2, 0, 2, 2], [2, 0, 2, 2, 2], [2, 0, 0, 0, 0], [0, 2, 0, 2, 0], [2, 2, 2, 0, 2], [2, 0, 2, 0, 0], [4, 4, 4, 4, 4], [2, 0, 0, 2, 2], [0, 0, 2, 0, 2], [2, 2, 0, 0, 0], [0, 0, 2, 0, 2], [0, 2, 0, 2, 2], [0, 2, 2, 0, 2]], "output": [[0, 3, 0, 0, 0], [3, 0, 0, 3, 0], [0, 3, 0, 0, 0], [0, 3, 3, 3, 3], [3, 0, 3, 3, 0], [3, 3, 0, 0, 3]]}, {"input": [[0, 2, 0, 2, 0], [2, 2, 0, 2, 2], [0, 2, 2, 2, 0], [0, 2, 2, 0, 0], [0, 2, 2, 2, 2], [2, 0, 2, 0, 2], [4, 4, 4, 4, 4], [2, 0, 2, 2, 2], [0, 2, 2, 0, 0], [2, 0, 2, 0, 2], [2, 0, 0, 0, 2], [2, 2, 0, 2, 0], [2, 0, 2, 2, 0]], "output": [[3, 3, 3, 0, 3], [3, 0, 3, 3, 3], [3, 3, 0, 3, 3], [3, 3, 3, 0, 3], [3, 0, 3, 0, 3], [0, 0, 0, 3, 3]]}]}
+const puzzle3 = {"train": [{"input": [[0, 0, 1, 1, 1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 2, 0, 0, 0, 0, 0], [0, 0, 0, 0, 2, 0, 0, 0, 0, 0], [0, 0, 0, 0, 2, 0, 0, 0, 0, 0]], "output": [[0, 0, 1, 1, 1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 2, 1, 1, 0, 0, 0], [0, 0, 1, 1, 2, 1, 1, 0, 0, 0], [0, 0, 0, 0, 2, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]}, {"input": [[0, 0, 1, 1, 1, 1, 1, 1, 1, 0], [0, 0, 1, 1, 0, 1, 1, 1, 1, 0], [0, 0, 1, 1, 0, 1, 0, 1, 1, 0], [0, 0, 1, 1, 0, 1, 0, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 2, 0, 2, 0, 0, 0]], "output": [[0, 0, 1, 1, 1, 1, 1, 1, 1, 0], [0, 0, 1, 1, 2, 1, 1, 1, 1, 0], [0, 0, 1, 1, 0, 1, 2, 1, 1, 0], [0, 0, 1, 1, 0, 1, 2, 1, 1, 0], [0, 0, 0, 0, 0, 0, 2, 1, 1, 0], [0, 0, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]}, {"input": [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 0, 1, 1, 1, 1, 1, 1], [0, 1, 1, 0, 1, 1, 1, 1, 0, 1], [0, 1, 1, 0, 0, 1, 0, 1, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 2, 0, 2, 0], [0, 0, 0, 2, 2, 0, 2, 0, 2, 0], [0, 0, 0, 2, 2, 0, 2, 0, 2, 0]], "output": [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 2, 1, 1, 1, 1, 1, 1], [0, 1, 1, 2, 1, 1, 1, 1, 2, 1], [0, 1, 1, 2, 2, 1, 2, 1, 2, 1], [0, 0, 0, 0, 2, 0, 2, 0, 2, 0], [0, 0, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]}], "test": [{"input": [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 0, 1, 1, 1, 1, 1, 0, 1], [0, 1, 0, 1, 0, 1, 0, 1, 0, 1], [0, 1, 0, 1, 0, 0, 0, 1, 0, 1], [0, 0, 0, 1, 0, 0, 0, 0, 2, 1], [0, 0, 0, 0, 0, 0, 0, 0, 2, 0], [0, 0, 0, 0, 0, 2, 0, 0, 2, 0], [0, 0, 2, 0, 0, 2, 0, 0, 2, 0], [0, 0, 2, 0, 2, 2, 0, 0, 2, 0], [0, 0, 2, 0, 2, 2, 2, 0, 2, 0]], "output": [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 2, 1, 1, 1, 1, 1, 2, 1], [0, 1, 2, 1, 2, 1, 2, 1, 2, 1], [0, 1, 2, 1, 2, 2, 0, 1, 2, 1], [0, 0, 0, 1, 0, 2, 0, 0, 2, 1], [0, 0, 0, 0, 0, 2, 0, 0, 2, 0], [0, 0, 0, 0, 0, 2, 0, 0, 2, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]}]}
+
+async function complete (data, solve) {
+    displayPuzzle(data);
+    for (const item of data.train) {
+        const inputArray = item.input; 
+        const solvedArray = await solve(inputArray, false);
+        const expectedOutput = item.output;
+        if (JSON.stringify(solvedArray) === JSON.stringify(expectedOutput)) {
+            console.log('Solved array matches expected output');
+        } else {
+            console.log('Solved array does not match expected output');
+        }
+    }
+
+    for (const item of data.test) {
+        const inputArray = item.input;
+        const solvedArray = await solve(inputArray, true);
+        const expectedOutput = item.output;
+
+        if (JSON.stringify(solvedArray) === JSON.stringify(expectedOutput)) {
+            console.log('Solved array matches expected output');
+        } else {
+            console.log('Solved array does not match expected output');
+        }
+    };
+}
+
+async function solve1(grid, withDisplay) {
+    let upperGrid = [];
+    let bottomGrid = [];
+    let outputGrid = [];
+
+    for (let i = 0; i < grid.length; i++) {
+        if (grid[i][0] == 4) {
+            upperGrid = grid.slice(0, i);
+            bottomGrid = grid.slice(i + 1);
+            break;
+        }
+    }
+
+    for (let i = 0; i < upperGrid.length; i++) {
+        outputGrid[i] = [];
+        for (let j = 0; j < upperGrid[i].length; j++) {
+            if ((upperGrid[i][j] == 0 && bottomGrid[i][j] == 0) || (upperGrid[i][j] == 2 && bottomGrid[i][j] == 2)) {
+                outputGrid[i][j] = 0;
+            } else {
+                outputGrid[i][j] = 3;
+            }
+            if (withDisplay) {
+                displayGrid(grid, outputGrid);
+                await new Promise(resolve => setTimeout(resolve, 100));
+            }
+        }
+    }
+
+    return outputGrid;
+}
+
+async function solve2(grid, withDisplay) {
+    let gridCopy = JSON.parse(JSON.stringify(grid));
+    
+    let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]; // right, down, left, up
+    let directionIndex = 0;
+    let i = 0, j = 0;
+    grid[i][j] = 3;
+    let justTurned = false
+    while (true) {
+        
+        let nextI = i + directions[directionIndex][0];
+        let nextJ = j + directions[directionIndex][1];
+
+        let nextnextI = nextI + directions[directionIndex][0];
+        let nextnextJ = nextJ + directions[directionIndex][1];
+
+        if (nextI < 0 || nextI >= grid.length || nextJ < 0 || nextJ >= grid[0].length || (nextnextI >= 0 && nextnextJ >= 0 && nextnextI < grid.length && nextnextJ < grid[0].length && grid[nextnextI][nextnextJ] == 3)) {
+            directionIndex = (directionIndex + 1) % 4;
+            if (justTurned) {
+                break
+            }
+            justTurned = true
+            continue
+        } else {
+            justTurned = false
+            i = nextI;
+            j = nextJ;
+            if (grid[i][j] == 3) {
+                break
+            }
+            grid[i][j] = 3;
+            if (withDisplay) {
+                displayGrid(gridCopy, grid);
+                await new Promise(resolve => setTimeout(resolve, 100));
+            }
+        }
+    }
+
+    return grid;
+}
+
+async function solve3(grid, withDisplay) {
+    let gridCopy = JSON.parse(JSON.stringify(grid));
+
+    let moved = true;
+    while (moved) {
+        moved = false;
+        for (let i = 1; i < grid.length; i++) {
+            for (let j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] === 2 && grid[i - 1][j] === 0) {
+                    grid[i][j] = 0;
+                    grid[i - 1][j] = 2;
+                    moved = true;
+                    if (withDisplay) {
+                        displayGrid(gridCopy, grid);
+                        await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+                }
+            }
+        }
+    }
+    return grid;
+}
+
+document.getElementById('startSolve1').addEventListener('click', () => {
+    complete(puzzle1, solve1);
+});
+
+document.getElementById('startSolve2').addEventListener('click', () => {
+    complete(puzzle2, solve2);
+});
+
+document.getElementById('startSolve3').addEventListener('click', () => {
+    complete(puzzle3, solve3);
+});
+
+function displayGrid(gridInput, grid) {
+    const gridInputDiv = document.getElementById('gridInput');
+    gridInputDiv.innerHTML = ''; // Clear previous grid
+    
+    gridInput.forEach(row => {
+        const rowDiv = document.createElement('div');
+        rowDiv.className = 'row';
+        row.forEach(cell => {
+            const cellDiv = document.createElement('div');
+            cellDiv.className = 'cell cell-' + cell; // Assign class based on cell value
+            rowDiv.appendChild(cellDiv);
+        });
+        gridInputDiv.appendChild(rowDiv);
+    });
+
+    const gridDiv = document.getElementById('grid');
+    gridDiv.innerHTML = ''; // Clear previous grid
+
+    grid.forEach(row => {
+        const rowDiv = document.createElement('div');
+        rowDiv.className = 'row';
+        row.forEach(cell => {
+            const cellDiv = document.createElement('div');
+            cellDiv.className = 'cell cell-' + cell; // Assign class based on cell value
+            rowDiv.appendChild(cellDiv);
+        });
+        gridDiv.appendChild(rowDiv);
+    });
+}
+
+function displayPuzzle(puzzle) {
+    const puzzleDiv = document.getElementById('puzzle');
+    puzzleDiv.innerHTML = ''; // Clear previous puzzle
+
+    puzzle.train.forEach((item, index) => {
+        const inputGrid = item.input;
+        const outputGrid = item.output;
+
+        // Create a container for the example
+        const exampleDiv = document.createElement('div');
+        exampleDiv.className = 'puzzle-example';
+
+        // Title for the example
+        const titleDiv = document.createElement('div');
+        titleDiv.className = 'puzzle-title';
+        titleDiv.textContent = `Example ${index + 1}:`;
+        exampleDiv.appendChild(titleDiv);
+
+        // Display input grid
+        const inputDiv = document.createElement('div');
+        inputDiv.className = 'puzzle-grid';
+        inputGrid.forEach(row => {
+            const rowDiv = document.createElement('div');
+            rowDiv.className = 'row';
+            row.forEach(cell => {
+                const cellDiv = document.createElement('div');
+                cellDiv.className = 'cell cell-' + cell; // Assign class based on cell value
+                rowDiv.appendChild(cellDiv);
+            });
+            inputDiv.appendChild(rowDiv);
+        });
+        exampleDiv.appendChild(inputDiv);
+
+        // Display output grid
+        const outputDiv = document.createElement('div');
+        outputDiv.className = 'puzzle-grid';
+        outputGrid.forEach(row => {
+            const rowDiv = document.createElement('div');
+            rowDiv.className = 'row';
+            row.forEach(cell => {
+                const cellDiv = document.createElement('div');
+                cellDiv.className = 'cell cell-' + cell; // Assign class based on cell value
+                rowDiv.appendChild(cellDiv);
+            });
+            outputDiv.appendChild(rowDiv);
+        });
+        exampleDiv.appendChild(outputDiv);
+
+        // Append the example to the puzzleDiv
+        puzzleDiv.appendChild(exampleDiv);
+    });
+}
